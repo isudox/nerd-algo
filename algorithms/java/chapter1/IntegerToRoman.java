2441f2ae72de0602ff8ac83573976926744c7ecf
sudoz <me@isudox.com>
2016-06-28 23:44:14 +0800

    Integer To Roman

---
+++ algorithms/java/chapter1/IntegerToRoman.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public String intToRoman(int num) {
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[][] roman = {ones, tens, hundreds, thousands};
+        String res = "";
+        int len = String.valueOf(num).length();
+        for (int i = len - 1; i >= 0; i--) {
+            int x = num / pow(10, i);
+            res = res + roman[i][x];
+            num = num % pow(10, i);
+        }
+        return res;
+    }
+
+    private int pow(int x, int y) {
+        int z = 1;
+        for (; y > 0; y--) z *= x;
+        return z;
+    }
+}
