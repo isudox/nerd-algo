7a5e81021bb8f03adf476f1a9047276fffeb0ca2
sudoz <isudox@gmail.com>
2019-01-17 02:59:21 +0800

    replace junit with spock

---
+++ algorithms/java-solution/test/SolutionsTest.groovy
@@ -0,0 +1,116 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2019 sudoz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+import common.ListNode
+import spock.lang.Specification
+import utils.Converter
+
+/**
+ * Test algorithms with incomplete cases by Spock.
+ */
+class SolutionsTest extends Specification {
+
+    void setup() {
+        // do something
+    }
+
+    void cleanup() {
+        // do something
+    }
+
+    /**
+     * 1. Two Sum
+     */
+    def "two sum"(int[] nums, int target, int[] result) {
+        given:
+        def solution = new TwoSum()
+
+        expect:
+        solution.twoSum1(nums, target) == result
+
+        where:
+        nums                           | target | result
+        [1, 5, 8, 10, 23, 99] as int[] | 31     | [2, 4] as int[]
+        [1, 5, 8, 10, 23, 99] as int[] | 100    | [0, 5] as int[]
+        [-1, 0, 5, 10, 15, 8] as int[] | 9      | [0, 3] as int[]
+        [-1, -3, -5, 10, 100] as int[] | 110    | [3, 4] as int[]
+    }
+
+    /**
+     * 2. Add Two Numbers
+     */
+    def "add two numbers"() {
+        given:
+        def solution = new AddTwoNumbers()
+        def arr1 = [2, 4, 3] as int[]
+        def arr2 = [5, 6, 4] as int[]
+        def l1 = Converter.convertListNode(arr1)
+        def l2 = Converter.convertListNode(arr2)
+
+        when:
+        def res1 = solution.addTwoNumbers1(l1, l2)
+        def res2 = solution.addTwoNumbers2(l1, l2)
+        def expected = Converter.convertListNode([7, 0, 8] as int[])
+
+        then:
+        Converter.convertArray(expected) == Converter.convertArray(res1)
+        Converter.convertArray(expected) == Converter.convertArray(res2)
+    }
+
+    /**
+     * 3. Longest Substring Without Repeating Characters
+     */
+    def "longest substring without repeating characters"() {
+        given:
+        def solution = new LongestSubstringWithoutRepeatingCharacters()
+
+        when:
+        def res = solution.lengthOfLongestSubstring("pwwkew")
+
+        then:
+        res == 3
+    }
+
+    /**
+     * 23. Merge k Sorted Lists
+     */
+    def "merge k sorted lists"() {
+        given:
+        def solution = new MergeKSortedLists()
+        def i1 = [1, 4, 5] as int[]
+        def i2 = [1, 3, 4] as int[]
+        int[] i3 = [2, 6]
+        def l1 = Converter.convertListNode(i1)
+        def l2 = Converter.convertListNode(i2)
+        def l3 = Converter.convertListNode(i3)
+
+        when:
+        def res = solution.mergeKLists([l1, l2, l3] as ListNode[])
+
+        then:
+        Converter.convertArray(res) == [1, 1, 2, 3, 4, 4, 5, 6] as int[]
+    }
+
+}
