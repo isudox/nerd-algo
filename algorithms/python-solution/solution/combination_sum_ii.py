27d03a557436928706c68841b99389f3cd5f3b03
isudox <isudox@gmail.com>
2019-04-01 21:27:13 +0800

    Complete 40. Combination Sum II

---
+++ algorithms/python-solution/solution/combination_sum_ii.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""40. Combination Sum II
+https://leetcode.com/problems/combination-sum-ii/
+
+Given a collection of candidate numbers (candidates) and
+a target number (target), find all unique combinations in candidates where
+the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+"""
+from typing import List
+
+
+class Solution:
+    def combination_sum2(self, candidates: List[int], target: int) \
+            -> List[List[int]]:
+        ans = []
+        candidates.sort()
+
+        def backtrack(nums: List[int], targ: int, start: int, store: List[int]):
+            for i in range(start, len(nums)):
+                num = nums[i]
+                if targ < num:
+                    break
+
+                store.append(num)
+                if targ == num:
+                    nonlocal ans
+                    ans.append(store[:])
+                    store.pop()
+                    break
+
+                backtrack(nums, targ - num, i + 1, store)
+                store.pop()
+
+        backtrack(candidates, target, 0, [])
+
+        return [list(x) for x in set(tuple(x) for x in ans)]
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.combination_sum2([10, 1, 2, 7, 6, 1, 5], 8))
+    print(solution.combination_sum2([2, 5, 2, 1, 2], 5))
--- algorithms/python-solution/test/test_solution.py
+++ algorithms/python-solution/test/test_solution.py
@@ -378,3 +378,18 @@ def test_combination_sum(candidates, target, expect):
     solution = Solution()
 
     assert solution.combination_sum(candidates, target) == expect
+
+
+@pytest.mark.parametrize("candidates, target, expect", [
+    ([10, 1, 2, 7, 6, 1, 5], 8, [
+        [2, 6], [1, 1, 6], [1, 2, 5], [1, 7]
+    ]),
+    ([2, 5, 2, 1, 2], 5, [
+        [1, 2, 2], [5]
+    ]),
+])
+def test_combination_sum_2(candidates, target, expect):
+    from solution.combination_sum_ii import Solution
+    solution = Solution()
+
+    assert solution.combination_sum2(candidates, target) == expect
