582c73b76cb879467e7c3037674860e79ef6a839
isudox <isudox@gmail.com>
2019-06-09 02:23:37 +0800

    98. Validate Binary Search Tree Submit my solution of in-order traversal.

--- python-algorithm/leetcode/unique_binary_search_trees_ii.py
+++ python-algorithm/leetcode/unique_binary_search_trees_ii.py
@@ -25,17 +25,69 @@ The above output corresponds to the 5 unique BST's shown below:
 ⁠   /     /       \                 \
 ⁠  2     1         2                 3
 """
+import copy
 from typing import List
 
 from common.tree_node import TreeNode
 
 
 class Solution:
-    def generate_trees(self, n: int) -> List[TreeNode]:
-        assert n > 0
+    def generate_trees_1(self, n: int) -> List[TreeNode]:
+        """
+        recursive
+        :param n:
+        :return:
+        """
+        if n == 0:
+            return []
         if n == 1:
             return [TreeNode(1)]
-        ans = []
 
-        return ans
+        def backtrack(left: int, right: int) -> List[TreeNode]:
+            if left > right:
+                return [None]
+            if left == right:
+                return [TreeNode(left)]
+            trees = []
+            for i in range(left, right + 1):
+                for left_tree in backtrack(left, i - 1):
+                    for right_tree in backtrack(i + 1, right):
+                        root = TreeNode(i)
+                        root.left = left_tree
+                        root.right = right_tree
+                        trees.append(root)
+            return trees
 
+        return backtrack(1, n)
+
+    def generate_trees_2(self, n: int) -> List[TreeNode]:
+        """
+        dp
+        :param n:
+        :return:
+        """
+
+        def modify_tree(node: TreeNode, offset: int):
+            if offset == 0 or not node:
+                return
+            node.val += offset
+            modify_tree(node.left, offset)
+            modify_tree(node.right, offset)
+
+        # dp[i] stores the trees of n=i
+        if n == 0:
+            return []
+        dp = [[] for _ in range(n + 2)]
+        dp[0].append(None)
+        dp[1].append(TreeNode(1))
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                for left_tree in dp[j - 1]:
+                    for right_tree in dp[i - j]:
+                        root = TreeNode(j)
+                        root.left = left_tree
+                        new_right_tree = copy.deepcopy(right_tree)
+                        modify_tree(new_right_tree, j)
+                        root.right = new_right_tree
+                        dp[i].append(root)
+        return dp[n]
--- python-algorithm/leetcode/validate_binary_search_tree.py
+++ python-algorithm/leetcode/validate_binary_search_tree.py
@@ -5,12 +5,11 @@ Given a binary tree, determine if it is a valid binary search tree (BST).
 
 Assume a BST is defined as follows:
 
-
-The left subtree of a node contains only nodes with keys less than the node's
-key.
-The right subtree of a node contains only nodes with keys greater than the
-node's key.
-Both the left and right subtrees must also be binary search trees.
+- The left subtree of a node contains only nodes with keys less than
+the node's key.
+- The right subtree of a node contains only nodes with keys greater than
+the node's key.
+- Both the left and right subtrees must also be binary search trees.
 
 
 Example 1:
@@ -45,12 +44,33 @@ class Solution:
         def dfs(node: TreeNode, minimum, maximum) -> bool:
             if not node:
                 return True
-            if node.val <= minimum or node.val >= maximum:
+            if minimum is not None and minimum >= node.val:
                 return False
-            if not dfs(node.left, minimum, node.val):
+            if maximum is not None and maximum <= node.val:
                 return False
-            if not dfs(node.right, node.val, maximum):
-                return False
-            return True
+            return dfs(node.left, minimum, node.val) and dfs(node.right,
+                                                             node.val, maximum)
 
-        return dfs(root, float('-inf'), float('inf'))
+        return dfs(root, None, None)
+
+    def is_valid_bst_inorder(self, root: TreeNode) -> bool:
+        """
+        BST's in-order traversal sequence must be incremental.
+        :param root:
+        :return:
+        """
+        seq = []
+        stack = []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                top_ele = stack.pop()
+                seq.append(top_ele.val)
+                if top_ele.right:
+                    root = top_ele.right
+        for i in range(1, len(seq)):
+            if seq[i - 1] >= seq[i]:
+                return False
+        return True
