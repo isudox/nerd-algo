b7698858d77a7240f2a1053bbc106d74579451f0
isudox <isudox@gmail.com>
2020-12-30 12:25:34 +0800

    Submit LeetCode No.330 & 1046

---
+++ python-algorithm/leetcode/problem_330.py
@@ -0,0 +1,45 @@
+"""330. Patching Array
+https://leetcode.com/problems/patching-array/
+
+Given a sorted positive integer array nums and an integer n,
+add/patch elements to the array such that any number in range [1, n]
+inclusive can be formed by the sum of some elements in the array.
+Return the minimum number of patches required.
+
+Example 1:
+
+Input: nums = [1,3], n = 6
+Output: 1
+Explanation:
+Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
+Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
+Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
+So we only need 1 patch.
+
+Example 2:
+
+Input: nums = [1,5,10], n = 20
+Output: 2
+Explanation: The two patches can be [2, 4].
+
+Example 3:
+
+Input: nums = [1,2,2], n = 5
+Output: 0
+"""
+from typing import List
+
+
+class Solution:
+    def min_patches(self, nums: List[int], n: int) -> int:
+        size, i = len(nums), 0
+        x = 1
+        ans = 0
+        while x <= n:
+            if i < size and nums[i] <= x:
+                x += nums[i]
+                i += 1
+            else:
+                x = x * 2
+                ans += 1
+        return ans
