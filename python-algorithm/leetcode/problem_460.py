e976769b108de36b0c191c2d56e45d3500132af3
isudox <isudox@gmail.com>
2021-06-01 19:15:02 +0800

    Submit LeetCode No.460

--- python-algorithm/leetcode/problem_460.py
+++ python-algorithm/leetcode/problem_460.py
@@ -1,38 +1,69 @@
 """460. LFU Cache
 https://leetcode.com/problems/lfu-cache/
 
-Design and implement a data structure for Least Frequently Used (LFU) cache.
-It should support the following operations: get and put.
+Design and implement a data structure for a Least Frequently Used (LFU)
+cache.
 
-get(key) - Get the value (will always be positive) of the key if the key
-exists in the cache, otherwise return -1.
-put(key, value) - Set or insert the value if the key is not already present.
-When the cache reaches its capacity, it should invalidate the least frequently
-used item before inserting a new item. For the purpose of this problem,
-when there is a tie (i.e., two or more keys that have the same frequency),
-the least recently used key would be evicted.
+Implement the LFUCache class:
 
-Note that the number of times an item is used is the number of calls to the get
-and put functions for that item since it was inserted. This number is set to
-zero when the item is removed.
+LFUCache(int capacity) Initializes the object with the capacity of the data
+structure.
+int get(int key) Gets the value of the key if the key exists in the cache.
+Otherwise, returns -1.
+void put(int key, int value) Update the value of the key if present, or
+inserts the key if not already present. When the cache reaches its capacity,
+it should invalidate and remove the least frequently used key before
+inserting a new item. For this problem, when there is a tie (i.e., two or
+more keys with the same frequency), the least recently used key would be
+invalidated.
 
-Follow up:
-Could you do both operations in O(1) time complexity?
+To determine the least frequently used key, a use counter is maintained for
+each key in the cache. The key with the smallest use counter is the least
+frequently used key.
 
-Example:
+When a key is first inserted into the cache, its use counter is set to 1 (due
+to the put operation). The use counter for a key in the cache is incremented
+either a get or put operation is called on it.
 
-LFUCache cache = new LFUCache( 2 /* capacity */ );
+Example 1:
 
-cache.put(1, 1);
-cache.put(2, 2);
-cache.get(1);       // returns 1
-cache.put(3, 3);    // evicts key 2
-cache.get(2);       // returns -1 (not found)
-cache.get(3);       // returns 3.
-cache.put(4, 4);    // evicts key 1.
-cache.get(1);       // returns -1 (not found)
-cache.get(3);       // returns 3
-cache.get(4);       // returns 4
+Input
+["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get",
+"get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+
+Explanation
+// cnt(x) = the use counter for key x
+// cache=[] will show the last used order for tiebreakers (leftmost element
+is  most recent)
+LFUCache lfu = new LFUCache(2);
+lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+lfu.get(1);      // return 1
+⁠                // cache=[1,2], cnt(2)=1, cnt(1)=2
+lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest,
+invalidate 2.
+// cache=[3,1], cnt(3)=1, cnt(1)=2
+lfu.get(2);      // return -1 (not found)
+lfu.get(3);      // return 3
+⁠                // cache=[3,1], cnt(3)=2, cnt(1)=2
+lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate
+1.
+⁠                // cache=[4,3], cnt(4)=1, cnt(3)=2
+lfu.get(1);      // return -1 (not found)
+lfu.get(3);      // return 3
+⁠                // cache=[3,4], cnt(4)=1, cnt(3)=3
+lfu.get(4);      // return 4
+⁠                // cache=[3,4], cnt(4)=2, cnt(3)=3
+
+Constraints:
+
+0 <= capacity, key, value <= 10^4
+At most 10^5 calls will be made to get and put.
+
+Follow up: Could you do both operations in O(1) time complexity? 
 """
 
 
@@ -40,71 +71,31 @@ class LFUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.store = {}
-        self.sorted_keys = []
-        self.tree_map = {}
+        self.counter = dict()
+        self.timestamp = 0
+        self.store = dict()
 
     def get(self, key: int) -> int:
         if key not in self.store:
             return -1
-        self.sorted_keys.remove(key)
-        self.sorted_keys.insert(0, key)
-        self.tree_map[key] = self.tree_map[key] + 1
-        self.tree_map = self._sort_by_value(self.tree_map)
+        self.timestamp += 1
+        self.counter[key][0] += 1
+        self.counter[key][1] = self.timestamp
         return self.store[key]
 
     def put(self, key: int, value: int) -> None:
         if self.capacity == 0:
             return
-        if key not in self.store:
-            if len(self.store) == self.capacity:
-                lfu_value = list(self.tree_map.values())[0]
-                lfu_keys = []
-                for k, v in self.tree_map.items():
-                    if v == lfu_value:
-                        lfu_keys.append(k)
-                for k in reversed(self.sorted_keys):
-                    if k in lfu_keys:
-                        self.sorted_keys.remove(k)
-                        self.sorted_keys.insert(0, key)
-                        del self.store[k]
-                        self.store[key] = value
-                        del self.tree_map[k]
-                        self.tree_map[key] = 0
-                        break
-            else:
-                self.sorted_keys.insert(0, key)
-                self.store[key] = value
-                self.tree_map[key] = 0
-        else:
-            self.sorted_keys.remove(key)
-            self.sorted_keys.insert(0, key)
-            self.store[key] = value
-            self.tree_map[key] = self.tree_map[key] + 1
+        if key not in self.store and len(self.store) == self.capacity:
+            lfu_key, lfu_cnt, lfu_ts = None, 100000, 100000
+            for k, cnt in self.counter.items():
+                if cnt[0] < lfu_cnt or cnt[0] == lfu_cnt and cnt[1] < lfu_ts:
+                    lfu_cnt = cnt[0]
+                    lfu_ts = cnt[1]
+                    lfu_key = k
+            del self.counter[lfu_key]
+            del self.store[lfu_key]
+        self.timestamp += 1
+        self.counter[key] = [(self.counter[key][0] if key in self.store else 0) + 1, self.timestamp]
+        self.store[key] = value
 
-        self.tree_map = self._sort_by_value(self.tree_map)
-
-    def _sort_by_value(self, unsorted_map):
-        return {k: v for k, v in
-                sorted(unsorted_map.items(), key=lambda item: item[1])}
-
-
-if __name__ == '__main__':
-    # [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
-    cache = LFUCache(2)
-    cache.put(1, 1)
-    cache.put(2, 2)
-    print(cache.get(1) == 1)  # 1
-    cache.put(3, 3)
-    print(cache.get(2) == -1)  # -1
-    print(cache.get(3) == 3)  # 3
-    cache.put(4, 4)
-    print(cache.get(1) == -1)  # -1
-    print(cache.get(3) == 3)  # 3
-    print(cache.get(4) == 4)  # 4
-
-    # ["LFUCache","put","get"]
-    # [[0],[0,0],[0]]
-    cache1 = LFUCache(0)
-    cache1.put(0, 0)
-    print(cache1.get(0) == -1)
