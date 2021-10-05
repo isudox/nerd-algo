6951b5063e002ae580ea589b0bf2e980382c59ca
isudox <isudox@gmail.com>
2021-10-05 14:15:32 +0800

    Submit LeetCode 70

--- go-algorithm/leetcode/problem463.go
+++ go-algorithm/leetcode/problem463.go
@@ -3,31 +3,20 @@ package leetcode
 // 463. Island Perimeter
 // https://leetcode.com/problems/island-perimeter/
 func islandPerimeter(grid [][]int) int {
-	m, n := len(grid), len(grid[0])
-	seen := make([][]bool, m)
-	for i := 0; i < m; i++ {
-		seen[i] = make([]bool, n)
-	}
+	dirs := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 	ans := 0
-	dirs := [4][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-	var bfs func(x, y int)
-	bfs = func(x, y int) {
-		if seen[x][y] {
-			return
-		}
-		seen[x][y] = true
-		for _, dir := range dirs {
-			nx, ny := x+dir[0], y+dir[1]
-			if grid[x][y] == 1 {
-				if (nx < 0 || nx == m || ny < 0 || ny == n) || grid[nx][ny] == 0 {
-					ans++
+	m, n := len(grid), len(grid[0])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == 1 {
+				for _, d := range dirs {
+					nx, ny := i+d[0], j+d[1]
+					if nx < 0 || nx == m || ny < 0 || ny == n || grid[nx][ny] == 0 {
+						ans++
+					}
 				}
 			}
-			if 0 <= nx && nx < m && 0 <= ny && ny < n && !seen[nx][ny] {
-				bfs(nx, ny)
-			}
 		}
 	}
-	bfs(0, 0)
 	return ans
 }
---
+++ go-algorithm/leetcode/problem70.go
@@ -0,0 +1,14 @@
+package leetcode
+
+// 70. Climbing Stairs
+// https://leetcode.com/problems/climbing-stairs/
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := []int{1, 2}
+	for i := 2; i < n; i++ {
+		dp[1], dp[0] = dp[0] + dp[1], dp[1]
+	}
+	return dp[1]
+}
