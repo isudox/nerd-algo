e35a8df57c9adebd84a05f8aa0f9bd50f265efa0
isudox <isudox@gmail.com>
2021-11-01 00:33:24 +0800

    Submit LeetCode 575.

---
+++ go-algorithm/leetcode/problem575.go
@@ -0,0 +1,19 @@
+package leetcode
+
+// 575. Distribute Candies
+// https://leetcode.com/problems/distribute-candies/
+
+func distributeCandies(candyType []int) int {
+	seen := make(map[int]bool)
+	types := 0
+	for _, ct := range candyType {
+		if !seen[ct] {
+			types++
+			seen[ct] = true
+		}
+	}
+	if n := len(candyType) / 2; n < types {
+		return n
+	}
+	return types
+}
