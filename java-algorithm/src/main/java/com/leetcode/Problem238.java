b6d057422d25e06bf56f3fc982d8ef2f679de1ee
isudox <isudox@gmail.com>
2021-11-27 20:25:53 +0800

    Submit LeetCode No.238 & 519

---
+++ java-algorithm/src/main/java/com/leetcode/Problem238.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 238. Product of Array Except Self
+ * https://leetcode.com/problems/product-of-array-except-self/
+ */
+public class Problem238 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        int[] right = new int[nums.length];
+        right[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = left[i] * right[i];
+        }
+        return nums;
+    }
+}
