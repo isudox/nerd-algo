d37739d7118aa7cc5bcef5cf45af892567a0dccc
isudox <isudox@gmail.com>
2021-12-19 17:49:31 +0800

    Submit LeetCode No.997

---
+++ python-algorithm/leetcode/problem_997.py
@@ -0,0 +1,17 @@
+"""997. Find the Town Judge
+https://leetcode.com/problems/find-the-town-judge/
+"""
+from typing import List
+
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        store = list(range(n + 1))
+        for t0, t1, in trust:
+            store[t1] += t0
+            store[t0] -= t1
+        total = (1 + n) * n // 2
+        for k, v in enumerate(store):
+            if v == total:
+                return k
+        return -1
