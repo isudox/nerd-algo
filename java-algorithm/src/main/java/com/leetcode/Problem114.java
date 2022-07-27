93e1605f6b4de3d53e48a9c98a813b105830f0b5
isudox <isudox@gmail.com>
2022-07-27 19:04:34 +0800

    Submit LeetCode No.114

---
+++ java-algorithm/src/main/java/com/leetcode/Problem114.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+import com.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem114 {
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        List<TreeNode> list = new ArrayList<>();
+        recur(root, list);
+        for (int i = 0; i < list.size(); i++) {
+            list.get(i).left = null;
+            if (i == list.size() - 1) break;
+            list.get(i).right = list.get(i + 1);
+        }
+    }
+
+    private void recur(TreeNode node, List<TreeNode> list) {
+        if (node == null) return;
+        list.add(node);
+        recur(node.left, list);
+        recur(node.right, list);
+    }
+
+    public static void main(String[] args) {
+        Problem114 p = new Problem114();
+        TreeNode root = new TreeNode(0, new TreeNode(1), new TreeNode(2));
+        p.flatten(root);
+    }
+}
