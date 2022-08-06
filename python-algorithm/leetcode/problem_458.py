162ef057bc3e56850674090459921307397fc7d5
isudox <isudox@gmail.com>
2022-08-06 17:50:05 +0800

    Submit LeetCode No.458

---
+++ python-algorithm/leetcode/problem_458.py
@@ -0,0 +1,11 @@
+"""458. Poor Pigs
+https://leetcode.com/problems/poor-pigs/
+"""
+
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        pigs = 0
+        while (minutesToTest // minutesToDie + 1) ** pigs < buckets:
+            pigs += 1
+        return pigs
