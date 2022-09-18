2cee7423f7e4f525e8d6b73614440758517c7295
isudox <isudox@gmail.com>
2022-09-18 15:02:52 +0800

    Submit LeetCode No.827

--- java-algorithm/src/main/java/com/leetcode/Problem42.java
+++ java-algorithm/src/main/java/com/leetcode/Problem42.java
@@ -1,12 +1,11 @@
 package com.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 42. Trapping Rain Water
  * https://leetcode.com/problems/trapping-rain-water/description/
- *
  *
  * Given n non-negative integers representing an elevation map where the width
  * of each bar is 1, compute how much water it is able to trap after raining.
@@ -14,11 +13,9 @@ import java.util.List;
  * ![img](https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png)
  *
  * The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
- * In this case, 6 units of rain water (blue section) are being trapped. Thanks
- * Marcos for contributing this image!
+ * In this case, 6 units of rain water (blue section) are being trapped.
  *
  * Example:
- *
  *
  * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
  * Output: 6
@@ -28,40 +25,30 @@ public class Problem42 {
     public int trap(int[] height) {
         int ans = 0;
         int left = 0;
-        List<Integer> stack = new ArrayList<>();
-
+        Deque<Integer> stack = new ArrayDeque<>();
         for (int h : height) {
-            if (stack.isEmpty()) {
-                stack.add(h);
-                left = h;
-            } else if (h < left) {
-                if (h <= stack.get(stack.size() - 1)) {
-                    stack.add(h);
-                } else {
-                    int cnt = 0;
-                    while (stack.get(stack.size() - 1) < h) {
-                        ans += h - stack.remove(stack.size() - 1);
-                        cnt++;
-                        if (stack.isEmpty()) {
-                            break;
-                        }
-                    }
-                    while (cnt >= 0) {
-                        stack.add(h);
-                        cnt--;
-                    }
-                }
-            } else {
-                int top = Math.min(left, h);
-                for (int s : stack) {
-                    ans += top - s;
-                }
+            if (stack.isEmpty() || h >= left) {
+                for (int s : stack) ans += left - s;
                 stack.clear();
                 stack.add(h);
                 left = h;
+                continue;
+            }
+            if (h <= stack.peekLast()) {
+                stack.add(h);
+            } else {
+                int cnt = 0;
+                while (stack.peekLast() < h) {
+                    ans += h - stack.removeLast();
+                    cnt++;
+                    if (stack.isEmpty()) break;
+                }
+                while (cnt >= 0) {
+                    stack.add(h);
+                    cnt--;
+                }
             }
         }
-
         return ans;
     }
 }
---
+++ java-algorithm/src/main/java/com/leetcode/Problem827.java
@@ -0,0 +1,107 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 827. Making A Large Island
+ * https://leetcode.com/problems/making-a-large-island/
+ */
+public class Problem827 {
+    private int[] parent;
+    private int[] size;
+
+    public int largestIsland(int[][] grid) {
+        int n = grid.length;
+        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+//        parent = new int[n * n + 1];
+//        size = new int[n * n + 1];
+//        for (int i = 1; i <= n * n; i++) {
+//            parent[i] = i;
+//            size[i] = 1;
+//        }
+        UnionFind uf = new UnionFind(n * n + 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) continue;
+                for (int[] d : dirs) {
+                    int x = i + d[0], y = j + d[1];
+                    if (0 <= x && x < n && 0 <= y && y < n && grid[x][y] != 0) {
+                        uf.union(i * n + j + 1, x * n + y + 1);
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    ans = Math.max(ans, uf.size[uf.find(i * n + j + 1)]);
+                } else {
+                    int cur = 1;
+                    Set<Integer> seen = new HashSet<>();
+                    for (int[] d : dirs) {
+                        int x = i + d[0], y = j + d[1];
+                        if (0 <= x && x < n && 0 <= y && y < n && grid[x][y] != 0) {
+                            int root = uf.find(x * n + y + 1);
+                            if (seen.contains(root)) continue;
+                            seen.add(root);
+                            cur += uf.size[root];
+                        }
+                    }
+                    ans = Math.max(ans, cur);
+                }
+            }
+        }
+        return ans;
+    }
+
+    private void union(int x, int y) {
+        int px = find(x), py = find(y);
+        if (px == py)
+            return;
+        if (size[px] > size[py]) {
+            union(y, x);
+        } else {
+            size[py] += size[px];
+            parent[px] = parent[py];
+        }
+    }
+
+    private int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private static class UnionFind {
+        private final int[] parent;
+        private final int[] size;
+
+        public UnionFind(int size) {
+            this.parent = new int[size];
+            this.size = new int[size];
+            for (int i = 1; i < size; i++) {
+                this.parent[i] = i;
+                this.size[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            int px = find(x), py = find(y);
+            if (px == py) return;
+            if (size[px] < size[py]) {
+                int tmp = px;
+                px = py;
+                py = tmp;
+            }
+            size[px] += size[py];
+            parent[py] = px;
+        }
+    }
+}
