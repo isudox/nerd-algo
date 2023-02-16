9ecd8e1ef8e957f46650e7ae60a4f44e50b200ce
isudox <isudox@gmail.com>
2023-02-16 23:51:37 +0800

    Submit LeetCode 989

--- python-algorithm/leetcode/problem_989.py
+++ python-algorithm/leetcode/problem_989.py
@@ -1,46 +1,25 @@
 """989. Add to Array-Form of Integer
 https://leetcode.com/problems/add-to-array-form-of-integer/
-
-For a non-negative integer X, the array-form of X is an array of its digits
-in left to right order.  For example, if X = 1231, then the array form is [1,2,3,1].
-
-Given the array-form A of a non-negative integer X, return the array-form of the integer X+K.
-
-Example 1:
-
-Input: A = [1,2,0,0], K = 34
-Output: [1,2,3,4]
-Explanation: 1200 + 34 = 1234
-
-Example 2:
-
-Input: A = [2,7,4], K = 181
-Output: [4,5,5]
-Explanation: 274 + 181 = 455
-
-Example 3:
-
-Input: A = [2,1,5], K = 806
-Output: [1,0,2,1]
-Explanation: 215 + 806 = 1021
-
-Example 4:
-
-Input: A = [9,9,9,9,9,9,9,9,9,9], K = 1
-Output: [1,0,0,0,0,0,0,0,0,0,0]
-Explanation: 9999999999 + 1 = 10000000000
-
-Note：
-
-    1 <= A.length <= 10000
-    0 <= A[i] <= 9
-    0 <= K <= 10000
-    If A.length > 1, then A[0] != 0
 """
 from typing import List
 
 
 class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        def merge(a: List[int], b: List[int]) -> List[int]:
+            add = 0
+            for i in range(len(a)):
+                cur = a[len(a) - 1 - i] + add + (b[len(b) - 1 - i] if i < len(b) else 0)
+                add, cur = divmod(cur, 10)
+                a[len(a) - 1 - i] = cur
+            return [1] + a if add else a
+
+        k_num = []
+        while k:
+            k_num.insert(0, k % 10)
+            k //= 10
+        return merge(num, k_num) if len(num) >= len(k_num) else merge(k_num, num)
+
     def add_to_array_form(self, a: List[int], k: int) -> List[int]:
         def helper(long_list: List[int], short_list: List[int]) -> List[int]:
             len_short, len_long = len(short_list), len(long_list)
@@ -86,3 +65,4 @@ class Solution:
         else:
             a = [0] * (len_k - len_a) + a
         return helper(a, list_k)
+
