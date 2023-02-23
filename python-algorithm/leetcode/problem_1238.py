190c99c05dbee7406337f62fa2ec6f095281e981
isudox <isudox@gmail.com>
2023-02-23 21:32:51 +0800

    Submit LeetCode 1238

---
+++ python-algorithm/leetcode/problem_1238.py
@@ -0,0 +1,34 @@
+"""1238. Circular Permutation in Binary Representation
+https://leetcode.com/problems/circular-permutation-in-binary-representation/
+"""
+from typing import List
+
+
+class Solution:
+    def circularPermutation(self, n: int, start: int) -> List[int]:
+        def next(x: int) -> int:
+            y = x
+            for i in range(n):
+                if (x >> i) & 1 == 1:
+                    z = y - (1 << i)
+                else:
+                    z = y + (1 << i)
+                if not seen[z]:
+                    return z
+            return 0
+
+        limit = 2 ** n
+        seen = [False] * limit
+        seen[start] = True
+        ans = [start]
+        for i in range(1, limit):
+            tmp = next(ans[-1])
+            seen[tmp] = True
+            ans.append(tmp)
+        return ans
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.circularPermutation(2, 3))
+    print(sol.circularPermutation(3, 2))
