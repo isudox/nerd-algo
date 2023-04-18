7516ddab8f71f1019b89580cd798e33914b84098
isudox <isudox@gmail.com>
2023-04-18 19:17:22 +0800

    Submit LeetCode 1026, 1768

--- java-algo/src/main/java/com/leetcode/Problem1026.java
+++ java-algo/src/main/java/com/leetcode/Problem1026.java
@@ -4,7 +4,7 @@ import com.common.TreeNode;
 
 /**
  * 1026. Maximum Difference Between Node and Ancestor
- * https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/description/
+ * https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
  */
 class Problem1026 {
     private int ans = 0;
@@ -21,14 +21,45 @@ class Problem1026 {
         if (node == null) {
             return;
         }
-        ans = getMax(ans, Math.abs(node.val - curMax), Math.abs(node.val - curMin));
+        ans = Math.max(ans, Math.max(Math.abs(node.val - curMax), Math.abs(node.val - curMin)));
         curMax = Math.max(curMax, node.val);
         curMin = Math.min(curMin, node.val);
         helper(node.left, curMax, curMin);
         helper(node.right, curMax, curMin);
     }
 
-    private int getMax(int a, int b, int c) {
-        return Math.max(a, Math.max(b, c));
+    public int maxAncestorDiff2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return ans;
+    }
+
+    // find the minimum & maximum val of current TreeNode
+    private int[] dfs(TreeNode node) {
+        int max = node.val, min = node.val;
+        if (node.left == null && node.right == null) {
+            return new int[]{max, min};
+        }
+        if (node.left != null) {
+            int[] leftArr = dfs(node.left);
+            max = Math.max(max, leftArr[0]);
+            min = Math.min(min, leftArr[1]);
+            int diff = Math.max(Math.abs(leftArr[0] - node.val), Math.abs(leftArr[1] - node.val));
+            if (diff > ans) {
+                ans = diff;
+            }
+        }
+        if (node.right != null) {
+            int[] rightArr = dfs(node.right);
+            max = Math.max(max, rightArr[0]);
+            min = Math.min(min, rightArr[1]);
+            int diff = Math.max(Math.abs(rightArr[0] - node.val), Math.abs(rightArr[1] - node.val));
+            if (diff > ans) {
+                ans = diff;
+            }
+        }
+        return new int[]{max, min};
     }
 }
--- java-algo/src/main/java/com/leetcode/Problem1768.java
+++ java-algo/src/main/java/com/leetcode/Problem1768.java
@@ -1,5 +1,9 @@
 package com.leetcode;
 
+/**
+ * 1768. Merge Strings Alternately
+ * https://leetcode.com/problems/merge-strings-alternately/
+ */
 public class Problem1768 {
     public String mergeAlternately(String word1, String word2) {
         int n = Math.min(word1.length(), word2.length());
@@ -7,6 +11,6 @@ public class Problem1768 {
         for (int i = 0; i < n; i++) {
             sb.append(word1.charAt(i)).append(word2.charAt(i));
         }
-        return sb + (word1.length() > n ? word1.substring(n) : word2.substring(n));
+        return sb.append(word1.length() > n ? word1.substring(n) : word2.substring(n)).toString();
     }
 }
