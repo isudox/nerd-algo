ef61e14b07b7c663c6dc2271eadbd8c952f098a5
isudox <isudox@gmail.com>
2023-07-31 00:48:04 +0800

    Submit LeetCode 143

--- python-algo/leetcode/problem_664.py
+++ python-algo/leetcode/problem_664.py
@@ -1,32 +1,5 @@
 """664. Strange Printer
 https://leetcode.com/problems/strange-printer/
-
-There is a strange printer with the following two special properties:
-
-The printer can only print a sequence of the same character each time.
-At each turn, the printer can print new characters starting from and ending
-at any place and will cover the original existing characters.
-
-Given a string s, return the minimum number of turns the printer needed to
-print it.
-
-Example 1:
-
-Input: s = "aaabbb"
-Output: 2
-Explanation: Print "aaa" first and then print "bbb".
-
-Example 2:
-
-Input: s = "aba"
-Output: 2
-Explanation: Print "aaa" first and then print "b" from the second place of
-the string, which will cover the existing character 'a'.
-
-Constraints:
-
-1 <= s.length <= 100
-s consists of lowercase English letters.
 """
 import collections
 import bisect
@@ -35,14 +8,13 @@ import bisect
 class Solution:
     def strange_printer(self, s: str) -> int:
         # remove the consecutive same char.
-        s = ''.join([c for i, c in enumerate(s) if i == 0 or s[i] != s[i - 1]])
-        n = len(s)
+        ss = [c for i, c in enumerate(s) if i == 0 or s[i] != s[i - 1]]
+        n = len(ss)
         dp = [[n] * n for _ in range(n)]
         for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if j == i:
-                    dp[i][j] = 1
-                elif s[j] == s[i]:
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if ss[j] == ss[i]:
                     dp[i][j] = dp[i][j - 1]
                 else:
                     for k in range(i, j):
