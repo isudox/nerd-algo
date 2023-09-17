883bcdbac67ee92653c878f1a33ff79bbbde7dd4
isudox <isudox@gmail.com>
2023-09-17 22:10:20 +0800

    Submit LeetCode 213, 1631

--- python-algo/leetcode/problem_1631.py
+++ python-algo/leetcode/problem_1631.py
@@ -6,34 +6,30 @@ from typing import List
 
 
 class Solution:
-    def minimum_effort_path(self, heights: List[List[int]]) -> int:
-        def dfs(x: int, y: int, diff: int):
-            memo[x][y] = True
-            if x == rows - 1 and y == cols - 1:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        def dfs(x: int, y: int, d: int):
+            visited[x][y] = True
+            if x == m - 1 and y == n - 1:
                 return
-            for move_x, move_y in dirs:
-                next_x, next_y = x + move_x, y + move_y
-                if 0 <= next_x < rows and 0 <= next_y < cols and not memo[next_x][next_y]:
-                    if abs(heights[next_x][next_y] - heights[x][y]) <= diff:
-                        dfs(next_x, next_y, diff)
+            for dx, dy in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny] \
+                    and abs(heights[nx][ny] - heights[x][y]) <= d:
+                    dfs(nx, ny, d)
 
-        rows, cols = len(heights), len(heights[0])
-        minimum, maximum = sys.maxsize, 0
-        for i in range(rows):
-            for j in range(cols):
-                if heights[i][j] < minimum:
-                    minimum = heights[i][j]
-                if heights[i][j] > maximum:
-                    maximum = heights[i][j]
-        max_diff, min_diff = maximum - minimum, 0
-        memo = [[False] * cols for _ in range(rows)]
-        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        while min_diff < max_diff:
-            memo = [[False] * cols for _ in range(rows)]
-            mid_diff = (min_diff + max_diff) // 2
-            dfs(0, 0, mid_diff)
-            if memo[-1][-1]:
-                max_diff = mid_diff
+        m, n = len(heights), len(heights[0])
+        mi = ma = heights[0][0]
+        for row in heights:
+            for v in row:
+                mi = min(mi, v)
+                ma = max(ma, v)
+        lo, hi = 0, ma - mi
+        while lo < hi:
+            visited = [[False] * n for _ in range(m)]
+            mid = (lo + hi) // 2
+            dfs(0, 0, mid)
+            if visited[-1][-1]:
+                hi = mid
             else:
-                min_diff = mid_diff + 1
-        return min_diff
+                lo = mid + 1
+        return lo
--- python-algo/leetcode/problem_213.py
+++ python-algo/leetcode/problem_213.py
@@ -1,29 +1,5 @@
 """213. House Robber II
 https://leetcode.com/problems/house-robber-ii/
-
-You are a professional robber planning to rob houses along a street.
-Each house has a certain amount of money stashed. All houses at this place
-are arranged in a circle. That means the first house is the neighbor of the
-last one. Meanwhile, adjacent houses have security system connected and it
-will automatically contact the police if two adjacent houses were broken
-into on the same night.
-
-Given a list of non-negative integers representing the amount of money of
-each house, determine the maximum amount of money you can rob tonight
-without alerting the police.
-
-Example 1:
-
-Input: [2,3,2]
-Output: 3
-Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
-             because they are adjacent houses.
-Example 2:
-
-Input: [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-             Total amount you can rob = 1 + 3 = 4.
 """
 from typing import List
 
@@ -49,3 +25,17 @@ class Solution:
             return nums[0]
         memo = [[-1] * 2 for _ in range(len(nums))]
         return max(dfs(0, 1), dfs(1, 0))
+
+    def rob2(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp0 = [[0, 0] for _ in range(n)]
+        for i in range(1, n):
+            dp0[i][0] = max(dp0[i - 1])
+            dp0[i][1] = dp0[i - 1][0] + nums[i]
+        ans = max(dp0[-1])
+        dp1 = [[0, 0] for _ in range(n)]
+        dp1[0] = (0, nums[0])
+        for i in range(1, n):
+            dp1[i][0] = max(dp1[i - 1])
+            dp1[i][1] = dp1[i - 1][0] + (0 if i == n - 1 else nums[i])
+        return max(ans, max(dp1[-1]))
