833a3d9cb82a8019f99a5188f2f39869b261391a
isudox <isudox@gmail.com>
2023-09-18 19:09:42 +0800

    Submit LeetCode 1337,337

--- python-algo/leetcode/problem_1337.py
+++ python-algo/leetcode/problem_1337.py
@@ -7,26 +7,14 @@ from typing import List
 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        def count(nums: List[int]) -> int:
-            cnt = 0
-            for num in nums:
-                if num == 0:
-                    break
-                cnt += 1
-            return cnt
-
-        values = [False] * (len(mat[0]) + 1)
-        store = collections.defaultdict(list)
-        for i in range(len(mat)):
-            cur = count(mat[i])
-            store[cur].append(i)
-            values[cur] = True
+        store = [[] for _ in range(len(mat[0]) + 1)]
+        for i, row in enumerate(mat):
+            store[sum(row)].append(i)
         ans = []
-        for i, value in enumerate(values):
-            if value:
-                rows = store[i]
-                for row in rows:
-                    if k > 0:
-                        ans.append(row)
-                        k -= 1
+        for i in range(len(mat[0]) + 1):
+            if store[i]:
+                ans.extend(store[i][:min(len(store[i]), k)])
+                k -= len(store[i])
+                if k <= 0:
+                    break
         return ans
--- python-algo/leetcode/problem_337.py
+++ python-algo/leetcode/problem_337.py
@@ -1,60 +1,20 @@
 """337. House Robber III
 https://leetcode.com/problems/house-robber-iii/
-
-The thief has found himself a new place for his thievery again.
-There is only one entrance to this area, called the "root." Besides the root,
-each house has one and only one parent house. After a tour, the smart thief
-realized that "all houses in this place forms a binary tree".
-It will automatically contact the police if two directly-linked houses were
-broken into on the same night.
-
-Determine the maximum amount of money the thief can rob tonight without
-alerting the police.
-
-Example 1:
-
-Input: [3,2,3,null,3,null,1]
-
-     3
-    / \
-   2   3
-    \   \
-     3   1
-
-Output: 7
-Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
-
-Example 2:
-
-Input: [3,4,5,1,3,null,1]
-
-     3
-    / \
-   4   5
-  / \   \
- 1   3   1
-
-Output: 9
-Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
 """
 from common.tree_node import TreeNode
+from typing import Optional
+import functools
 
 
 class Solution:
-    def rob(self, root: TreeNode) -> int:
-        def dfs(node: TreeNode) -> int:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        @functools.cache
+        def dfs(node: Optional[TreeNode], flag: bool) -> int:
             if not node:
                 return 0
-            if node in memo:
-                return memo.get(node)
-            temp1 = node.val
-            if node.left:
-                temp1 += dfs(node.left.left) + dfs(node.left.right)
-            if node.right:
-                temp1 += dfs(node.right.left) + dfs(node.right.right)
-            temp2 = dfs(node.left) + dfs(node.right)
-            memo[node] = max(temp1, temp2)
-            return memo[node]
+            ret = dfs(node.left, True) + dfs(node.right, True)
+            if flag:
+                ret = max(ret, dfs(node.left, False) + dfs(node.right, False) + node.val)
+            return ret
 
-        memo = {}
-        return dfs(root)
+        return max(dfs(root, True), dfs(root, False))
