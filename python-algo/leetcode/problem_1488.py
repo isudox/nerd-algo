dfe71f64b36783dad5c914aef952c9a26f20777d
isudox <isudox@gmail.com>
2023-10-16 19:34:52 +0800

    Submit LeetCode 260

---
+++ python-algo/leetcode/problem_1488.py
@@ -0,0 +1,9 @@
+"""
+https://leetcode.cn/problems/avoid-flood-in-the-city/
+"""
+from typing import List
+
+
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        return []
