356befa81adfd46c0cd15678f1759d17d0489129
isudox <isudox@gmail.com>
2023-10-29 22:18:41 +0800

    Submit LeetCode 274

---
+++ java-algo/src/main/java/com/leetcode/Problem274.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+/**
+ * 274. H-Index
+ * https://leetcode.com/problems/h-index
+ */
+public class Problem274 {
+    public int hIndex(int[] citations) {
+        int max = 0;
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int c : citations) {
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
+            max = Math.max(max, c);
+        }
+        int cnt = 0;
+        for (int i = max; i >= 1; i--) {
+            cnt += counter.getOrDefault(i, 0);
+            if (cnt >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
