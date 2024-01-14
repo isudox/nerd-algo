342df0a17c6775961060008b5527e8e2879dccb8
isudox <isudox@gmail.com>
2024-01-14 21:37:51 +0800

    submit leetcode 83

---
+++ java-algo/src/main/java/com/leetcode/Problem83.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+class Problem83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = head;
+        while (p != null && p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
