5f1b1dd8d77a929b1570cbbcb540242784a5d0b3
isudox <isudox@gmail.com>
2024-01-31 21:06:10 +0800

    submit leetcode 739, 931

--- java-algo/src/main/java/com/leetcode/Problem739.java
+++ java-algo/src/main/java/com/leetcode/Problem739.java
@@ -1,43 +1,25 @@
 package com.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 739. Daily Temperatures
  * https://leetcode.com/problems/daily-temperatures/
- *
- * Given a list of daily temperatures T, return a list such that, for each day
- * in the input, tells you how many days you would have to wait until a warmer
- * temperature. If there is no future day for which this is possible,
- * put 0 instead.
- *
- * For example, given the list of temperatures
- * T = [73, 74, 75, 71, 69, 72, 76, 73],
- * your output should be [1, 1, 4, 2, 1, 1, 0, 0].
- *
- * Note: The length of temperatures will be in the range [1, 30000].
- * Each temperature will be an integer in the range [30, 100].
  */
 public class Problem739 {
-
-    public int[] bruteFroce(int[] t) {
-        int[] result = new int[t.length];
-
-        for (int i = 0; i < t.length; i++) {
-            int count = 0;
-            for (int j = i; j < t.length; j++) {
-                if (t[j] > t[i]) {
-                    break;
-                }
-                count++;
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && t > temperatures[stack.peek()]) {
+                int preIdx = stack.pop();
+                ans[preIdx] = i - preIdx;
             }
-            if (i + count == t.length) {
-                result[i] = 0;
-            } else {
-                result[i] = count;
-            }
-
+            stack.push(i);
         }
-
-        return result;
+        return ans;
     }
-
 }
--- java-algo/src/main/java/com/leetcode/Problem931.java
+++ java-algo/src/main/java/com/leetcode/Problem931.java
@@ -6,6 +6,25 @@ package com.leetcode;
  */
 public class Problem931 {
     public int minFallingPathSum(int[][] matrix) {
-        return 0;
+        int ans = 9999999;
+        int n = matrix.length;
+        int[] dp0 = new int[n], dp1 = new int[n];
+        System.arraycopy(matrix[0], 0, dp0, 0, n);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp1[j] = dp0[j] + matrix[i][j];
+                if (j > 0) {
+                    dp1[j] = Math.min(dp1[j], dp0[j - 1] + matrix[i][j]);
+                }
+                if (j < n - 1) {
+                    dp1[j] = Math.min(dp1[j], dp0[j + 1] + matrix[i][j]);
+                }
+            }
+            System.arraycopy(dp1, 0, dp0, 0, n);
+        }
+        for (int num : dp0) {
+            ans = Math.min(ans, num);
+        }
+        return ans;
     }
 }
