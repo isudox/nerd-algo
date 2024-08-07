a6e3d7c98fa32fc3019a34a80ca287f26cf413a1
isudox <isudox@gmail.com>
2024-08-08 01:25:00 +0800

    submit leetcode 273

---
+++ java-algo/src/main/java/com/leetcode/Problem273.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+/**
+ * 273. Integer to English Words
+ * https://leetcode.com/problems/integer-to-english-words/
+ */
+public class Problem273 {
+    private static final String[] belowTen = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private static final String[] belowTwenty = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] belowHundred = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        return convertToWords(num);
+    }
+
+    private String convertToWords(int num) {
+        if (num < 10) {
+            return belowTen[num];
+        }
+        if (num < 20) {
+            return belowTwenty[num - 10];
+        }
+        if (num < 100) {
+            return belowHundred[num / 10] + (num % 10 != 0 ? " " + convertToWords(num % 10) : "");
+        }
+        if (num < 1000) {
+            return convertToWords(num / 100) + " Hundred" + (num % 100 != 0 ? " " + convertToWords(num % 100) : "");
+        }
+        if (num < 1000000) {
+            return convertToWords(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + convertToWords(num % 1000) : "");
+        }
+        if (num < 1000000000) {
+            return convertToWords(num / 1000000) + " Million" + (num % 1000000 != 0 ? " " + convertToWords(num % 1000000) : "");
+        }
+        return convertToWords(num / 1000000000) + " Billion" + (num % 1000000000 != 0 ? " " + convertToWords(num % 1000000000) : "");
+    }
+}
